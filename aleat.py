25d08c0d3380c811463e7f130973cae6ff757fc3
Borja Egea Madrid <boegma@hotmail.com>
2017-02-28 02:10:39 +0100

    Ejercicio 14.6 de URLsAleat orientado a objetos

--- aleat.py
+++ aleat.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# Borja Egea Madrid
+#!/usr/bin/python3
 
 import socket
 
@@ -17,28 +16,28 @@ class webApp:
 		  mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		  mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		  mySocket.bind((hostname, port))
-	
+
 		  # Queue a maximum of 5 TCP connection requests
 		  mySocket.listen(5)
-	
+
 		  # Accept connections, read incoming data, and call
 		  # parse and process methods (in a loop)
 
 		  while True:
-			   print 'Waiting for connections'
+			   print ('Waiting for connections')
 			   (recvSocket, address) = mySocket.accept()
-			   print 'HTTP request received (going to parse and process):'
+			   print ('HTTP request received (going to parse and process):')
 			   request = recvSocket.recv(2048)
-			   print request
+			   print (request)
 			   parsedRequest = self.parse(request)
 			   (returnCode, htmlAnswer) = self.process(parsedRequest)
-			   print 'Answering back...'
-			   recvSocket.send("HTTP/1.1 " + returnCode + " \r\n\r\n"
-					                  + htmlAnswer + "\r\n")
+			   print ('Answering back...')
+			   recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n"
+					                  + htmlAnswer + "\r\n", 'utf-8'))
 			   recvSocket.close()
-	
+
 class aleatApp(webApp):
-	
+
 	 def process(self, parsedRequest):
 		  import random
 		  respuesta = '<a href = "' + str(random.randint(1,1000000)) + '">Dame otra</a>'
@@ -46,4 +45,3 @@ class aleatApp(webApp):
 
 if __name__ == "__main__":
 	testWebApp = aleatApp("localhost", 1234)
-	
